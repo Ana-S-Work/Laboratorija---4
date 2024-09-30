f15287f53aa33ddc98e3c1ca267a417e57ec6155
ketchup-boy <63000307+ketchup-boy@users.noreply.github.com>
2024-09-30 15:42:54 +0200

    different tests for linreg models

--- lab4/R/linreg.R
+++ lab4/R/linreg.R
@@ -27,36 +27,53 @@
 
 
 LR_Class <- setRefClass(
+  Class = "LR_Class",
   fields = list(
-    formula = "formula",    # Formula object
+    model_formula = "formula",    
     data = "data.frame",
-    reg_coef = "matrix", # Matrix of estimated coefficients
-    fitted_values = "matrix", # Predicted values
+    reg_coef = "matrix", 
+    fitted_values = "matrix", 
     residual = "matrix",
-    degreees_of_freedome = "numeric",
+    degrees_of_freedome = "numeric",
     residual_var = "numeric", 
-    var_reg_coeff = "numeric",
+    var_reg_coef = "numeric",
     p_values = "numeric"
     
     
   ),
   methods = list(
-    init = function(formula, data) {
-      formula <<- formula
+    init = function(model_formula, data) {
+      model_formula <<- model_formula
       data <<- data
       .self$LR_fit()
     },
     LR_fit = function(){
-      X <- model.matrix(formula, data)
-      y <- data[[all.vars(formula)[1]]]
+      X <- model.matrix(model_formula, data)
+      y <- data[[all.vars(model_formula)[1]]]
       reg_coef <<- solve(t(X) %*% X) %*% t(X) %*% y
       fitted_values <<- X %*% reg_coef
       residual <<- y - fitted_values
-      degrees_of_freeedome <<- nrow(X) - ncol(X)
-      residual_var <<- sum(resid^2) / degrees_of_freeedome
-      var_beta <- residual_var * solve(t(X) %*% X)
-      t_coef <- reg_coef / sqrt(var_beta)
-      p_values <- 2 * (1 - pt(abs(t_coef), degrees_of_freeedome))
+      degrees_of_freedome <<- nrow(X) - ncol(X)
+      residual_var <<- sum(residual^2) / degrees_of_freeedome
+      var_reg_coef <<- residual_var * solve(t(X) %*% X)
+      t_coef <- reg_coef / sqrt(var_reg_coef)
+      p_values <<- 2 * (1 - pt(abs(t_coef), degrees_of_freeedome))
+    },
+    print = function(){
+      print(self$reg_coef)
+    },
+    summary = function() {
+      cat("Call:\n")
+      #print(self$model_formula)
+      cat("\nCoefficients:\n")
+      coef_table <- cbind(Estimate = reg_coef, "Std. Error" = var_reg_coef, "p-value" = p_values)
+      #print(coef_table)
+      cat("\nResidual standard error:", sqrt(residual_var), "on", degrees_of_freedome, "degrees of freedom\n")
     }
   )
 )
+
+data("mtcars")
+
+model <- LR_Class$new(model_formula = mpg ~ wt + hp, data = mtcars)
+model$summary()
--- lab4/R/y.R
+++ lab4/R/y.R
@@ -1,5 +1,86 @@
-y <-
-function(x){
-  z <- x+3
-  return(z)
-}
+# Define the RC class for multiple linear regression
+LinRegRC <- setRefClass(
+  "LinRegRC",  # Class name
+  fields = list(
+    formula = "formula",      # Formula object for the model
+    data = "data.frame",      # Data used for the regression
+    coefficients = "matrix",  # Coefficients (beta estimates)
+    fitted_values = "matrix", # Fitted values (predictions)
+    residuals = "matrix",     # Residuals (actual - predicted)
+    var_coefficients = "matrix",      # Standard errors of the coefficients
+    p_values = "matrix",     # P-values of the coefficients
+    df_residual = "numeric",  # Degrees of freedom for residuals
+    residual_variance = "numeric"        # Estimate of variance of residuals
+  ),
+  
+  methods = list(
+    # Initialize method (called when a new object is created)
+    initialize = function(formula, data) {
+      formula <<- formula
+      data <<- data
+      .self$fit_model()  # Call the fit_model method to fit the model
+    },
+    
+    # Method to fit the linear regression model
+    fit_model = function() {
+      X <- model.matrix(formula, data)    # Design matrix X (independent variables)
+      y <- data[[all.vars(formula)[1]]]   # Response vector y (dependent variable)
+      
+      # Calculate coefficients using the normal equation: (X'X)^(-1) X'y
+      coefficients <<- solve(t(X) %*% X) %*% t(X) %*% y
+      
+      # Predicted values (fitted values)
+      fitted_values <<- X %*% coefficients
+      
+      # Residuals (difference between actual and predicted values)
+      residuals <<- y - fitted_values
+      
+      # Degrees of freedom
+      df_residual <<- nrow(X) - ncol(X)
+      
+      # Estimate of variance 
+      residual_variance <<- sum(residuals^2) / df_residual
+      
+      # Variance-covariance matrix of the coefficients
+      var_coefficients <<- residual_variance * solve(t(X) %*% X)
+      
+      
+      # t-values for the coefficients
+      se_beta <- sqrt(diag(var_coefficients))
+      t_values <- coefficients / se_beta
+      
+      # Calculate p-values based on the t-distribution
+      p_values <<- 2 * (1 - pt(abs(t_values), df_residual))
+    },
+    
+    # Method to print the summary of the regression model
+    summary = function() {
+      cat("Call:\n")
+      print(formula)
+      cat("\nCoefficients:\n")
+      coef_table <- cbind(Estimate = coefficients, "var beta" = var_coefficients, "p-value" = p_values)
+      print(coef_table)
+      cat("\nResidual standard error:", sqrt(residual_variance), "on", df_residual, "degrees of freedom\n")
+    },
+    
+    # Method to predict new values based on new data
+    predict = function(newdata) {
+      X_new <- model.matrix(formula, newdata)  # Create design matrix for new data
+      return(X_new %*% coefficients)           # Return predicted values
+    },
+    
+    print = function(){
+      
+    }
+  )
+)
+
+# Load the mtcars dataset
+data(mtcars)
+
+# Create an instance of the LinRegRC class
+model <- LinRegRC$new(formula = mpg ~ wt + hp, data = mtcars)
+
+# Display the summary of the model
+model$summary()
+
